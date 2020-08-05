e7ac88a93e8ca13aeea22424122fbe650bcb2474
wanwenqq <wanwenqq@hotmail.com>
2020-08-05 17:11:30 +0800

    添加vant-list说明

--- vue-mint/src/views/Data.vue
+++ vue-mint/src/views/Data.vue
@@ -1,15 +1,58 @@
 <template>
-    <div>
-        data
-    </div>
+  <div>
+    <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad" @refresh="onRefresh">
+      <van-cell v-for="item in list" :key="item" :title="item" />
+    </van-list>
+  </div>
 </template>
 
 <script>
+import { List,Cell } from 'vant';
+
 export default {
-    
-}
+    components: {
+    [List.name]: List,
+    [Cell.name]: Cell,
+  },
+  data() {
+    return {
+      list: [],
+      loading: false,
+      finished: false,
+    };
+  },
+  methods: {
+    onLoad() {
+      // 异步更新数据
+      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+      setTimeout(() => {
+        if (this.refreshing) {
+          this.list = [];
+          this.refreshing = false;
+        }
+
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1);
+        }
+        this.loading = false;
+
+        if (this.list.length >= 40) {
+          this.finished = true;
+        }
+      }, 1000);
+    },
+     onRefresh() {
+      // 清空列表数据
+      this.finished = false;
+
+      // 重新加载数据
+      // 将 loading 设置为 true，表示处于加载状态
+      this.loading = true;
+      this.onLoad();
+    },
+  },
+};
 </script>
 
 <style lang="less">
-    
 </style>
