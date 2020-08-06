9f0db17870eef57b07ff07fd93770e90f5146bc8
wanwenqq <wanwenqq@hotmail.com>
2020-08-06 17:39:38 +0800

    测试swtc/lib

--- vue-mint/src/views/Home.vue
+++ vue-mint/src/views/Home.vue
@@ -1,17 +1,62 @@
 <template>
-    <div>
-        home
-    </div>
+  <div>
+    <van-button type="primary" @click="onCreateSWTC()">创建</van-button>
+    <van-button type="primary" @click="onrequestOrderBook()">挂单列表</van-button>
+  </div>
 </template>
 
 <script>
+//创建Wallet对象
+var jlib = require("@swtc/lib");
+var Wallet = jlib.Wallet;
+var Remote = jlib.Remote;
+//测试环境
+// var remote = new Remote({
+//   server: "ws://ts5.jingtum.com:5030",
+//   issuer: "jBciDE8Q3uJjf111VeiUNM775AMKHEbBLS",
+// });
+// 生产环境
+var remote = new Remote();
 
+import { Button } from "vant";
 
 export default {
-    
-}
+  name: "home",
+  components: {
+    [Button.name]: Button,
+  },
+  data() {
+    return {
+      title: "home",
+      address: "",
+    };
+  },
+  mounted() {},
+  methods: {
+    onCreateSWTC() {
+      var w1 = Wallet.generate();
+      this.address = w1["address"];
+      console.log(w1["address"]);
+    },
+    onrequestOrderBook() {
+      remote
+        .connectPromise()
+        .then(async () => {
+          let options = {
+            limit: 5,
+            pays: remote.makeCurrency(),
+            gets: remote.makeCurrency("CNY"),
+          };
+          let req = remote.requestOrderBook(options);
+          let response = await req.submitPromise();
+          console.log(response);
+          remote.disconnect();
+        })
+        .catch(console.error);
+    },
+  },
+};
 </script>
 
 <style lang="less">
-    
 </style>
